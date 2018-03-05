515e2a0f82e3b28eb504593a98282b6939a83a04
Chris <chrisieaks@gmail.com>
2018-03-05 11:21:39 -0500

    basic routes

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(app.get('port'), () => {
     console.log(`App is running at http://localhost:${PORT}`);
