4d3e9dcd3c7682849497bfbe2f276cc5b59fb53f
Roger McIntosh <rogermcintosh@Rogers-MacBook-Air.local>
2018-03-05 14:42:25 -0500

    update

--- javascript.js
+++ javascript.js
@@ -1,4 +1,11 @@
-$(document).ready(function(){
 
+//    Header clock
+
+window.onload = displayTime;
+function displayTime() {
+    var element = document.getElementById("clock");
+    var now = new Date();
+    element.innerHTML = now.toLocaleTimeString();
+    setTimeout(displayTime, 1000);
+}
     
-});
