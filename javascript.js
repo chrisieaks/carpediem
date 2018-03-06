0ee78030cb4533e723ddb992a8c1ba0c16187804
Roger McIntosh <rogermcintosh@Rogers-MacBook-Air.local>
2018-03-06 10:20:51 -0500

    update

--- javascript.js
+++ javascript.js
@@ -1,23 +1,30 @@
-
 //    Header clock
-
-window.onload = displayTime;
-function displayTime() {
-    var element = document.getElementById("clock");
-    var now = new Date();
-    element.innerHTML = now.toLocaleTimeString();
-    setTimeout(displayTime, 1000);
-}
-  
-
-// Exploding "Hi"
-function onMouseMove( event ) {
-    // Do nothing, based on scroll
-}
-$('.hover-block').hover(
-    function() {
-        explodeLetter($(this).parents('section').attr('id'), $(this).data('letter'), $(this).data('color'));
-    }, function() {
-        implodeLetter($(this).parents('section').attr('id'), $(this).data('letter'), $(this).data('color'));
+$(document).ready(function(){
+    function displayTime() {
+        var element = document.getElementById("clock");
+        var now = new Date();
+        element.innerHTML = now.toLocaleTimeString();
+        setTimeout(displayTime, 1000);
     }
-);
+    
+    function dislplayAlert(){
+        $('.modal').modal();
+        $('#modal1').modal('open');
+    }
+    displayTime();
+    dislplayAlert();
+    
+    
+    // Exploding "Hi"
+    function onMouseMove(event) {
+        // Do nothing, based on scroll
+    }
+    $('.hover-block').hover(
+        function () {
+            explodeLetter($(this).parents('section').attr('id'), $(this).data('letter'), $(this).data('color'));
+        },
+        function () {
+            implodeLetter($(this).parents('section').attr('id'), $(this).data('letter'), $(this).data('color'));
+        }
+    );
+})
