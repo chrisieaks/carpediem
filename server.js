e4055a3d8d0ad9540f43362b8304a5cc21349598
Yoon <Yoon@Yoons-MacBook-Pro-2.local>
2018-03-06 11:32:45 -0500

    commit version 68e9b4e

--- server.js
+++ server.js
@@ -1,19 +1,42 @@
+// NPM/Node Packages
 const path = require('path');
+const request = require('request');
 const express = require('express');
 const app = express();
 
+
+// Local Packages
 const keys = require('./keys.js');
 
+
+// Application Port
 const PORT = process.env.PORT || 3000;
 
 
+// Static file rendering
 app.use(express.static('./public'));
 
 
+// Routing
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.post('/', function(req, res){
+    let zipcode = req;
+    let queryURL = 'http://api.wunderground.com/api/6ada133f06c8d75d/conditions/q/' + zipcode +'.json';
+    
+    console.log(zipcode);
+    console.log(queryURL);
+    request({
+        url: queryURL,
+        json: true
+    }, function(error, response, body){
+        console.log(JSON.stringify(body, undefined, 2));
+    });
+});
+
+// Start Server
 app.listen(PORT, () => {
     console.log(`App is running at http://localhost:${PORT}`);
     console.log('  Press CTRL-C to stop\n');
