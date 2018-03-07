13e3ecbfd62d92fbc847f8071941a03f33f927ec
Chris <chrisieaks@gmail.com>
2018-03-07 10:04:51 -0500

    here goes nothing

--- javascript.js
+++ javascript.js
@@ -28,28 +28,8 @@ $(document).ready(function(){
             implodeLetter($(this).parents('section').attr('id'), $(this).data('letter'), $(this).data('color'));
         }
     );
-})
-$(document).ready(function(){
-
-    //Open tap-target on page load
-    // $('.tap-target').tapTarget('open');
-
-    //Close tap-target on widget add button hover
-    $('.btn-floating').on('mouseover', function(){
-        $('.tap-target').tapTarget('close');
-    });
-
-    $('#weather').on('click', function(){
-        let div = $('<div>');
-        div.addClass('col s12 m5');
-        $('#content').append(div);
-        div.append('<div class="card-panel grey">');
-        $('.card-panel').append('<span class="white-text">');
-    });
-
-    
-    
 });
+
 
 
 // $('#weather').on('click', function(){
